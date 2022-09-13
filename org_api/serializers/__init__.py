4caca386c1d8808055352ae1bdf495a103716ce1
Vicki Taylor <vtaylor85@live.com>
2022-09-13 11:11:24 -0500

    category list

--- org_api/serializers/__init__.py
+++ org_api/serializers/__init__.py
@@ -2,3 +2,4 @@ from .user_serializer import UserSerializer, OrganizerSerializer
 from .room_serializer import RoomSerializer
 from .event_serializer import EventSerializer
 from .item_serializer import ItemSerializer
+from .category_serializer import CategorySerializer
